3844f4225c6ba22bf1929a455500cb25733f86f6
Veaceslav Ciolac <vciolac.qa@gmail.com>
2024-09-02 14:30:30 +0300

    first commit

---
+++ closures.js
@@ -0,0 +1,18 @@
+let view;
+function initialize() {
+  let called = 0;
+  return function () {
+    if (called > 0) {
+      return;
+    } else {
+      view = "wall";
+      called++;
+      console.log("View has been set!");
+    }
+  };
+}
+const startOnce = initialize();
+startOnce()
+
+console.log(view);
+console.log(view);
